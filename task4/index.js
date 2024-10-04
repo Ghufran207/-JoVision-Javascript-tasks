52d39a38471d913b2d7e9c9149dc0dabe83c9b83
Ghufran207 <ghofranalawawdeh@gmail.com>
2024-10-04 15:38:51 +0300

    Add files via upload

---
+++ task4/index.js
@@ -0,0 +1,12 @@
+
+function createArray(){
+    let count=0;
+    const arr=[];
+    for(let i=0;i<=100;i++){
+        arr.push(i);
+    count+=arr[i];}
+    console.log("The sum of all numbers between 0 and 100 is:", count);
+
+   // console.log("hello world");
+}
+
